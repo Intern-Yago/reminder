7f234eb50bb3b92e4b9ccb91cc539285b6252b90
Intern-Yago <yagovictorbotafogo@gmail.com>
2021-10-27 20:42:15 -0300

    html dinamico

--- src/Timer.js
+++ src/Timer.js
@@ -8,6 +8,8 @@ const Timer = {
     timeToMinutes: time => Math.floor(time/60),
     timeToSeconds: time => time%60,
 
+    formatTime: time => String(time).padStart(2, "0"),
+
     init(){
         Timer.currentTimer = Timer.time
         Timer.interval = setInterval(Timer.countdown, 1000)
@@ -15,11 +17,16 @@ const Timer = {
 
     countdown(){
         Timer.currentTimer--
-        console.log(Timer.currentTimer)
+
+        const seconds = Timer.formatTime(Timer.timeToSeconds(Timer.currentTimer))
+        const minutes = Timer.formatTime(Timer.timeToMinutes(Timer.currentTimer))
+        
+        
+        console.log(`${Timer.formatTime(minutes)}:${Timer.formatTime(seconds)}`)
 
         View.render({
-            minutes: 25,
-            seconds: 10
+            minutes,
+            seconds
         })
 
         if(Timer.currentTimer === 0){
