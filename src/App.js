632dd2020533bfc6673d3ef4adc16f48e6cb312b
Intern-Yago <yagovictorbotafogo@gmail.com>
2021-10-29 19:57:31 -0300

    iniciando aplicacao com npm

--- src/App.js
+++ src/App.js
@@ -2,21 +2,19 @@ import { Notifyer } from "./Notifyer.js"
 import { Timer } from "./Timer.js"
 import { Emitter } from "./Emitter.js"
 
+const notify = Notifyer.notify({
+    title: "Recado EsPCEx",
+    body: "Acorda para vida e vai estudar, vagabundo"
+})
+
 const App = {
     async start(){
         try{
             await Notifyer.init()
 
-            Emitter.on("countdown-start",()=>{
-                Notifyer.notify({
-                    title: "Recado EsPCEx",
-                    body: "Acorda para vida e vai estudar, vagabundo"
-                })
-            })
+            Emitter.on("countdown-start",notify)
             
-            Emitter.on("countdown-end", ()=>{
-                Timer.init()
-            })
+            Emitter.on("countdown-end", Timer.init)
 
             Timer.init(0.2*60)
             
